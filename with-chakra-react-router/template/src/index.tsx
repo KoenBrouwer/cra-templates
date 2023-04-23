c4419e1734d854e377d59f75172dddb9564e26d0
Koen Brouwer <KoenBrouwer@users.noreply.github.com>
2023-04-23 21:25:15 +0200

    Updated to React 18

--- with-chakra-react-router/template/src/index.tsx
+++ with-chakra-react-router/template/src/index.tsx
@@ -1,20 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import {ChakraProvider} from "@chakra-ui/react";
+import {createRoot} from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import theme from "./theme";
 
-ReactDOM.render(
+createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<ChakraProvider theme={theme}>
 			<App />
 		</ChakraProvider>
-	</React.StrictMode>,
-	document.getElementById("root"),
+	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
